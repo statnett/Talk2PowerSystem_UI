5ed7ece13c78b4ea86f78b19291264e0b8c6d1b6
Antoniy Kunchev <antoniy.kunchev@ontotext.com>
2025-10-23 09:41:31 +0300

    Adds capability for context path provisioning via environment variable
    - We've changed the way `default.conf` for the Nginx is set. Now instead
    of having the file directly copied into the `nginx/conf.d/` directory,
    the file is provided as `default.conf.template`, which is processed by
    initialization scripts, when the Nginx is started.
    
      This change is need in order to be able to provide the application
    context path as environment variable, which is resolved and set in the
    template, before creating the actual `default.conf` file into the
    `nginx/conf.d/` directory.
    
      There are some changes in the configurations as well. Instead of
    having the `/usr/share/nginx/html/` as root, now we defined it as an
    alias.
    
    - We've added custom initialization script that is added to the Nginx.
    It will handle the setting of the new context path for the application,
    when it is changed. The script is called `context-path-adapter.sh`,
    which is added to the Nginx `/docker-entrypoint.d/` directory.
    
    - The Helm chart was updated in order to provide the value of the
    `configuration.contextPath` as environment variable to the container.

---
+++ docker/scripts/context-path-adapter.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -eu
+
+log() {
+  echo "$(basename $0): $1"
+}
+
+if [ "/chat/" == "$CONTEXT_PATH" ]; then
+  log "There are no changes in the context path. Skipping further processing."
+  exit 0
+fi
+
+INDEX_FILE=/usr/share/nginx/html/index.html
+CSS_BUNDLE=/usr/share/nginx/html/main.*.css
+
+if [ ! -e $INDEX_FILE ]; then
+  log "$INDEX_FILE was not found!"
+  exit 1
+fi
+
+if [ ! -e $CSS_BUNDLE ]; then
+  log "$CSS_BUNDLE was not found!"
+  exit 1
+fi
+
+sed -i "s|href=\"/chat/|href=\"$CONTEXT_PATH|g" $INDEX_FILE
+sed -i "s|src=\"/chat/|src=\"$CONTEXT_PATH|g" $INDEX_FILE
+sed -i "s|url(/chat/|url($CONTEXT_PATH|g" $CSS_BUNDLE
+
+log "The context path of the application was successfully set to $CONTEXT_PATH"
